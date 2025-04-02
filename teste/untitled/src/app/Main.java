7f24cf3977484dcd08ef3c97687c2b9de297df56
Guilherme <guilhermefloesmmarchi@gmail.com>
2025-04-02 18:49:35 -0300

    Foi retirado a classe Cliente

--- teste/untitled/src/app/Main.java
+++ teste/untitled/src/app/Main.java
@@ -1,14 +1,8 @@
 package app;
 
-import entities.Cliente;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Cliente cliente = new Cliente("Maria",20);
-        System.out.println("Nome: " + cliente.getName());
-        System.out.print("Idade: " + cliente.getIdade());
+        System.out.println("Ola");
     }
 }
