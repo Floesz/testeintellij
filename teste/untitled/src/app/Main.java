1a75758c2e2342ff86829df361e8110510726ae3
Guilherme <guilhermefloesmmarchi@gmail.com>
2025-04-02 18:53:09 -0300

    Adicionei um Scanner com duas perguntas

--- teste/untitled/src/app/Main.java
+++ teste/untitled/src/app/Main.java
@@ -1,8 +1,15 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um nome: ");
+        String name = sc.nextLine();
+        System.out.print("Digite um numero qualquer: ");
+        int n = sc.nextInt();
+        
 
-        System.out.println("Ola");
     }
 }
